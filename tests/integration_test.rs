7a81de88adcb2bf6c7b105051a45d8353fb490bc
Vincent Gauthier <pub@luxbulb.org>
2025-09-18 18:58:01 +0200

    add integration test

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,7 +1,7 @@
 use assert_cmd::prelude::*; // Add methods on commands
+use assert_fs::prelude::*; // Filesystem assertions
 use predicates::prelude::*; // Used for writing assertions
 use std::process::Command; // Run programs
-
 #[test]
 fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
     let mut cmd = Command::cargo_bin("webinfo")?;
@@ -11,5 +11,18 @@ fn file_doesnt_exist() -> Result<(), Box<dyn std::error::Error>> {
         .failure()
         .stderr(predicate::str::contains("Failed to open CSV file"));
 
+    Ok(())
+}
+
+#[test]
+fn process_csv_file() -> Result<(), Box<dyn std::error::Error>> {
+    let file = assert_fs::NamedTempFile::new("sample.txt")?;
+    file.write_str("origin,popularity,date,country\nhttps://www.free.fr,1000,2025-08-28,FR\n")?;
+
+    let mut cmd = Command::cargo_bin("webinfo")?;
+    cmd.arg("--csv").arg(file.path());
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("\"hostname\": \"www.free.fr\""));
     Ok(())
 }
