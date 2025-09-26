8d04bc558bca469df76a98d848a9c2add21030de
Vincent Gauthier <pub@luxbulb.org>
2025-09-26 08:04:28 +0200

    add progress bar

--- src/bin/webinfo.rs
+++ src/bin/webinfo.rs
@@ -1,17 +1,18 @@
 use anyhow::Result;
 use clap::Parser;
 use futures::future::try_join_all;
+use indicatif::{HumanCount, ProgressBar, ProgressStyle};
 use itertools::izip;
-use std::{
-    fs::File,
-    iter::repeat_with,
-    path::PathBuf,
-    sync::Arc,
-    time::{Duration, SystemTime},
-};
-use tokio::time::timeout;
+use std::{fs::File, io::BufRead, iter::repeat_with, path::PathBuf, sync::Arc, time::SystemTime};
 use tokio::{sync::mpsc, task::spawn};
 use webinfo::utils::chunked;
+
+fn count_lines(path: &str) -> Result<usize, std::io::Error> {
+    let mut lines = std::io::BufReader::new(File::open(&path)?).lines();
+    // Set breakpoint on the next line, pull the drive, then continue
+    let count = lines.try_fold(0, |acc, line| line.map(|_| acc + 1))?;
+    Ok(count)
+}
 
 // Look at best pratices
 // 1. https://youtu.be/XCrZleaIUO4?si=hDRLbn3wgZ2TqRuW
@@ -41,6 +42,7 @@ async fn run(
     mut rdr: csv::Reader<File>,
     tx: mpsc::Sender<Result<webinfo::IpInfo>>,
     chunk_size: usize,
+    total_lines: usize,
     custom_dns: Option<String>,
 ) -> Result<()> {
     // Initialize dns resolver
@@ -52,7 +54,13 @@ async fn run(
         .map_err(|e| anyhow::anyhow!("Failed to open ASN database: {}", e))?;
     let ip2asn_map = Arc::new(ip2asn_map);
     // Implement chunking to limit the number of concurrent tasks
-    let mut num_records_processed = 0;
+    let bar = ProgressBar::new(total_lines as u64);
+    bar.set_style(
+        ProgressStyle::with_template("[{bar:50.cyan/blue}] {msg}")
+            .unwrap()
+            .progress_chars("= "),
+    );
+    let mut progress = 0;
     for chunk in chunked(rdr.deserialize::<OriginRecord>(), chunk_size) {
         // store all task handles
         let mut handles = Vec::new();
@@ -73,15 +81,10 @@ async fn run(
             // Spawn a task
             let handle = spawn(async move {
                 // Perform the query
-                let ip_info = timeout(
-                    Duration::from_secs(120),
-                    IpInfo::from_record(record, r, ip2asn),
-                )
-                .await;
+                let ip_info = IpInfo::from_record(record, r, ip2asn).await;
                 let ip_info = match ip_info {
-                    Ok(Ok(info)) => Ok(info),
-                    Ok(Err(e)) => Err(e),
-                    Err(_) => Err(anyhow::anyhow!("Operation timed out")),
+                    Ok(info) => Ok(info),
+                    Err(e) => Err(anyhow::anyhow!("Error processing record: {}", e)),
                 };
                 let _ = sender.send(ip_info).await;
             });
@@ -91,14 +94,18 @@ async fn run(
         // Wait for the current batch of tasks to complete
         let _ = try_join_all(handles).await?;
 
-        // Optional: brief pause to avoid overwhelming resources
-        num_records_processed += chunk_size;
-        let elapsed = now.elapsed().unwrap_or(Duration::new(0, 0));
-        eprintln!(
-            "Total Processed Records: {} => processes batch of {} records in {:.2?}",
-            num_records_processed, chunk_size, elapsed
-        );
+        // Update progress bar
+        bar.inc(chunk_size as u64);
+        progress += chunk_size;
+        bar.set_message(format!(
+            "{}/{}, {} records processed in {:.2} seconds",
+            HumanCount(progress.try_into().unwrap()),
+            HumanCount(total_lines.try_into().unwrap()),
+            chunk_size,
+            now.elapsed().unwrap().as_secs_f64()
+        ));
     }
+    bar.finish();
     Ok(())
 }
 
@@ -121,6 +128,15 @@ fn handle_result(mut rx: mpsc::Receiver<Result<webinfo::IpInfo>>) {
 async fn main() -> Result<()> {
     let cli = Cli::parse();
     let csv_path = cli.csv;
+    let csv_path_str = csv_path
+        .to_str()
+        .ok_or_else(|| anyhow::anyhow!("Invalid CSV path"))?;
+    let line_count = count_lines(csv_path_str)?;
+
+    eprintln!(
+        "Info: Starting processing file: {:?} with {} lines",
+        csv_path, line_count
+    );
     // open the CSV file
     let rdr = csv::Reader::from_path(&csv_path)
         .map_err(|e| anyhow::anyhow!("Failed to open CSV file: {}", e))?;
@@ -132,6 +148,6 @@ async fn main() -> Result<()> {
     handle_result(rx);
 
     // process chunk_size records concurrently
-    run(rdr, tx, cli.chunk_size, cli.dns).await?;
+    run(rdr, tx, cli.chunk_size, line_count, cli.dns).await?;
     Ok(())
 }
